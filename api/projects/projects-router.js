3e030c4aff7c41c8d936fb7b45073c86447d33d2
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-05-28 15:35:23 -0700

    complete POST endpoint for projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,9 +3,11 @@ const express = require("express");
 const { projectToBody } = require("../../data/helpers/mappers");
 const Projects = require("./projects-model");
 const colors = require("colors");
+const {newProjectPayloadValidation} = require("./projects-middleware");
 const router = express.Router();
 
-/*1 done*/ router.get("/", (req, res) => {
+/* TEST 1,2 DONE */ 
+router.get("/", (req, res) => {
   Projects.get()
     .then((found) => {
       res.json(found);
@@ -18,49 +20,37 @@ const router = express.Router();
       });
     });
 });
-// /*2 done?*/router.get('/:id', async (req, res) => {
-//     try {
-//         const project = await Project.get(req.params.id);
-//         console.log('message from projects', project);
-//         res.json(project.id);
-//         // if (!post) {
-//         //     res.status(404).json({
-//         //         message: 'The post with the specified ID does not exist',
-//         //     })
-//         // } else {
-//         //   res.json(project);
-//         // }
-//     } catch (err) {
-//         // res.status(404).json({
-//         //     message: "The project with the specified information could not be found.",
-//         //     err: err.message,
-//         //     stack: err.stack,
-//         // })
 
-//     }
-// })
+/* TEST 3,4 */
 router.get("/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const project = await Projects.get(id);
-    //   console.log(colors.blue(project));
     if (project == null) {
       return res.status(404).json({ message: "does not exist" });
     }
-    res.status(200).json({ project, id: project.id });
+
+    res.status(200).json({
+      name: project.name,
+      description: project.description,
+      completed: project.completed,
+    });
   } catch (error) {
     res.status(500).json({ message: "status 500" });
   }
-  //   Projects.get(id)
-  //     .then((data) => {
-  //       res.status(200).json({data, id: data.id});
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({ message: "status 500" });
-  //     });
 });
 
-/*3*/ router.post("/", (req, res) => {});
+/* TEST 5,6,7 DONE */ 
+router.post("/", newProjectPayloadValidation, async (req, res) => {
+try {
+    const project = req.body;
+    const newProject = await Projects.insert(project) 
+    res.status(201).json(newProject)   
+} catch (error) {
+    res.status(500).json({ message: "status 500" });
+}
+});
+
 /*5*/ router.delete("/:id", (req, res) => {});
 /*4*/ router.put("/:id", (req, res) => {});
 
