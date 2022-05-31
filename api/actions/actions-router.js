3dc2185a78730c4ed77689023c2353597796ec98
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-05-31 15:29:05 -0700

    complete actions PUT endpoint.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -6,6 +6,9 @@ const {
 const Action = require("./actions-model");
 const { response } = require("express");
 const { getProjectActions } = require("../projects/projects-model");
+const {
+  updateProjectPayloadValidation,
+} = require("../projects/projects-middleware");
 const router = express.Router();
 
 /*  TEST 15,16 DONE */ router.get("/", (req, res) => {
@@ -34,7 +37,7 @@ router.get("/:id", checkActionIdExists, async (req, res) => {
     res.status(500).json({ message: "status 500" });
   }
 });
-/* TEST 19, 20, 21 */
+/* TEST 19, 20, 21 DONE */
 router.post("/", newActionPayloadValidation, async (req, res) => {
   try {
     const action = req.body;
@@ -45,9 +48,22 @@ router.post("/", newActionPayloadValidation, async (req, res) => {
   }
 });
 
-/* TEST 25,26 */
-router.delete("/:id", (req, res) => {});
-/* TEST 22,23,24 */
-router.put("/:id", (req, res) => {});
+/* TEST 25,26 DONE */
+router.delete("/:id", checkActionIdExists, async (req, res) => {
+  await Action.remove(req.params.id);
+  res.status(200).json({
+    message: `your action ${req.action.project_id} was deleted`,
+  });
+});
+/* TEST 22,23,24 DONE! */
+router.put(
+  "/:id",
+  checkActionIdExists,
+  newActionPayloadValidation,
+  async (req, res) => {
+    const updatedAction = await Action.update(req.params.id, req.body);
+    res.status(200).json(updatedAction);
+  }
+);
 
 module.exports = router;
